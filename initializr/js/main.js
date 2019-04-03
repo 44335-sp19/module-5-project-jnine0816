ae098161fe8e2d8f2a078da3f2d14398ddec46fb
Jacob Nine <s527243@nwmissouri.edu>
2019-04-03 08:19:03 -0500

    pro 5

---
+++ initializr/js/main.js
@@ -0,0 +1,146 @@
+function myFunction(){
+    var txt;
+    var person = prompt("What's my name?");
+    if (person == "Jacob" ){
+        txt = "You have answered correctly";
+       
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo").innerHTML = txt;
+    
+}
+
+function myFunction1(){
+    var txt;
+    var person = prompt("Where am I from");
+    if (person == "Greenwood" ){
+        txt = "You have answered correctly";
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo1").innerHTML = txt;
+    
+}
+
+function myFunction2(){
+    var txt;
+    var person = prompt("What's my favorite sports team");
+    if (person == "Chiefs" ){
+        txt = "You have answered correctly";
+         
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo2").innerHTML = txt;
+    
+}
+
+function myFunction3(){
+    var txt;
+    var person = prompt("What's my moms name?");
+    if (person == "Theresa" ){
+        txt = "You have answered correctly";
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo3").innerHTML = txt;
+    
+}
+
+function myFunction4(){
+    var txt;
+    var person = prompt("What's my dads name?");
+    if (person == "Ron" ){
+        txt = "You have answered correctly";
+         
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo4").innerHTML = txt;
+    
+}
+
+function myFunction5(){
+    var txt;
+    var person = prompt("What's my sisters name?");
+    if (person == "Emily" ){
+        txt = "You have answered correctly";
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo5").innerHTML = txt;
+    
+}
+
+function myFunction6(){
+    var txt;
+    var person = prompt("Who is my best friend");
+    if (person == "Nick Davis" ){
+        txt = "You have answered correctly";
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo6").innerHTML = txt;
+    
+}
+
+function myFunction7(){
+    var txt;
+    var person = prompt("Where is my best friend from?");
+    if (person == "Lees Summit" ){
+        txt = "You have answered correctly";
+         
+        
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo7").innerHTML = txt;
+    
+}
+
+function myFunction8(){
+    var txt;
+    var person = prompt("How old is he?");
+    if (person == "21" ){
+        txt = "You have answered correctly";
+         
+    }
+    else{
+     txt = "You have answered incorrertly";
+       
+    }
+    
+    document.getElementById("demo8").innerHTML = txt;
+    
+}
+
